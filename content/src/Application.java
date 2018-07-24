3ff35d3754cd5763b0fc41eeebb22bd0ff060c0f
hobbes09 <sourin.sutradhar@gmail.com>
2018-07-24 18:02:02 +0530

    Added basic files

---
+++ content/src/Application.java
@@ -0,0 +1,12 @@
+import day1.MergeSort;
+
+public class Application {
+
+    public static void main(String[] args) {
+
+        System.out.println("\nHello world!!!!\n");
+
+        MergeSort.execute();
+    }
+
+}
